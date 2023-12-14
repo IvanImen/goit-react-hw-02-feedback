6c4323de33634d31923453bc71160f2517a92b18
IvanImen <vanyagarret@gmail.com>
2023-12-14 03:05:36 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleClick = statsKey =>
+    this.setState(prev => ({
+      [statsKey]: prev[statsKey] + 1,
+    }));
+  render() {
+    const statsTitles = Object.keys(this.state);
+
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        {statsTitles.map(statsTitle => (
+          <button key={statsTitle} onClick={() => this.handleClick(statsTitle)}>
+            {statsTitle}
+          </button>
+        ))}
+        <h2>Statistics</h2>
+        <ul>
+          {statsTitles.map(statsTitle => (
+            <li key={statsTitle}>
+              {statsTitle} {this.state[statsTitle]}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
